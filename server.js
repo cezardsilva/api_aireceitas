95f36c366fa5f432453625752b772597bf09e21a
Cezar D Silva <cezardsilva@gmail.com>
2025-06-07 16:55:30 -0300

    ajuste cors em server.js

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ import cors from 'cors'
 const app = express()
 const PORT = 3001
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173', // Dev
+    'https://seu-front.vercel.app', // Produção
+    'https://*.vercel.app' // 👈 Libera TODOS subdomínios Vercel!
+  ]
+}));
 
 app.use(express.json())
 
